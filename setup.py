6557d98a1c218683a1c8e3ee83ac5c4564a21b64
WaqasAkram2510 <waqasakram2510@gmail.com>
2025-08-15 18:51:52 +0500

    Requirements file created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical_chatbot",
+    version = "0.1",
+    author = "Waqas Akram",
+    author_email="waqasakram2510@gmail.com",
+    description="A medical chatbot using LangChain and OpenAI",
+    packages=find_packages(),
+    install_requires=[],
+)
